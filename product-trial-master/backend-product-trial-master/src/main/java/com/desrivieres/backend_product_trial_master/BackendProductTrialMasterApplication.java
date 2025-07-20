8a70fe107cb7895b39c59bbec094b493d281b70f
arnaudsptzz <arnaud.desrivieres@gmail.com>
2025-07-20 22:02:12 +0200

    [Back] Ajout du projet Backend en Spring Boot avec une configuration de base

---
+++ product-trial-master/backend-product-trial-master/src/main/java/com/desrivieres/backend_product_trial_master/BackendProductTrialMasterApplication.java
@@ -0,0 +1,13 @@
+package com.desrivieres.backend_product_trial_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProductTrialMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProductTrialMasterApplication.class, args);
+	}
+
+}
